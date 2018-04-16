8757d9cdeb77561c8220ddef3fe8685c58e6c5b0
Amar Jasarbasic <amarjasarbasic@gmail.com>
2018-04-16 12:51:57 -0400

    added vim colorscheme loader in install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=".bash_aliases .bash_exports .bash_options .bash_prompt .bashrc .gitignore .gitconfig .gitattributes .hushlogin .inputrc .profile"
+files=".vimrc .bash_aliases .bash_exports .bash_options .bash_prompt .bashrc .gitignore .gitconfig .gitattributes .hushlogin .inputrc .profile"
 
 echo "Backing up files"
 mkdir -p ~/dotfiles_old
@@ -18,5 +18,10 @@ for file in $files; do
   ln -s ~/dotfiles/$file ~/$file
   echo "Linking $file"
 done
+
+echo "Adding color schemes to /.vim/colors"
+mkdir ~/.vim
+mkdir ~/.vim/colors
+cp colors/* ~/.vim/colors/.
 
 source ~/.profile
